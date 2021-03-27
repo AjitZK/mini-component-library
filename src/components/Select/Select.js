d35aeeff8f4461767f4a6b686e64f23ec50a9170
AjitZK <ajitkrishna99@gmail.com>
2021-03-27 11:01:54 +0800

    💄 Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalSelect>
+        {displayedValue}
+        <IconWrapper style={{ "--icon-size": 24 + "px" }}>
+          <Icon id={"chevron-down"} size={24} strokeWidth={2} />
+        </IconWrapper>
+      </PresentationalSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationalSelect = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: 1rem;
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 2px auto Highlight;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--icon-size);
+  height: var(--icon-size);
+  pointer-events: none;
+`;
 
 export default Select;
